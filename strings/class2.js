9166da05c721a560e5f6b22d25c72e74e517e2bd
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-09-09 15:12:10 -0300

    Strings

---
+++ strings/class2.js
@@ -0,0 +1,8 @@
+var nombre = 'Jimmy',
+  apellido = 'Ibanez';
+var nombreEnMayusculas = nombre.toUpperCase();
+var apellidoEnMinusculas = apellido.toLowerCase();
+var primeraLetraDelNombre = nombre.charAt(0);
+var cantidadDeLetrasDelNombre = nombre.length;
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`;
+var str = nombre.substr(1, 2);
