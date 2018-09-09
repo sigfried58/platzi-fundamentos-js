1a703d9620395ca7fe82fdbc6d86cd3a927d6e31
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-09-09 14:53:56 -0300

    Variables

---
+++ variables/class1.js
@@ -0,0 +1,8 @@
+var name = 'Jimmy',
+  lastName = 'Ibanez';
+var age = 28;
+age = '28 años';
+console.log('Hi ' + name + ' ' + lastName);
+console.log('I am ' + age + ' years old');
+var weight = 75;
+weight = '75kg';
