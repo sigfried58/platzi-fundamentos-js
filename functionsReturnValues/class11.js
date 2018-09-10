6f06abc116a990470297424924c5512d90683528
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-09-10 00:54:16 -0300

    Functions that return values

---
+++ functionsReturnValues/class11.js
@@ -0,0 +1,30 @@
+var sacha = {
+  nombre: 'Sacha',
+  apellido: 'Lifszyc',
+  edad: 28,
+  ingeniero: false,
+  cocinero: false,
+  cantante: true,
+  dj: false,
+  guitarrista: false,
+  drone: false
+};
+var juan = {
+  nombre: 'Juan',
+  apellido: 'Gomez',
+  edad: 13
+};
+
+const MAYORIA_DE_EDAD = 18;
+
+function esMayorDeEdad(persona) {
+  return persona.edad >= MAYORIA_DE_EDAD;
+}
+
+function imprimirSiEsMayorDeEdad(persona) {
+  if (esMayorDeEdad(persona)) {
+    console.log(`${persona.nombre} es mayor de edad`);
+  } else {
+    console.log(`${persona.nombre} es menor de edad`);
+  }
+}
