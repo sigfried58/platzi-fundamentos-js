03abfbe11ac25102f15eba3e5164108c94a707af
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-09-10 00:24:24 -0300

    Comparisons

---
+++ comparisons/class9.js
@@ -0,0 +1,16 @@
+var x = 4,
+  y = '4';
+
+var sacha = {
+  nombre: 'Sacha'
+};
+var otraPersona = {
+  nombre: 'Sacha'
+};
+
+// var otraPersona = sacha
+//
+console.log(x == y); // true
+console.log(x === y); // false
+
+console.log(sacha == otraPersona);
