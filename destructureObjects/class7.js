a8c1c6a9554782f3596744d0f56db5ecb0564696
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-09-09 23:52:05 -0300

    Destructure objects

---
+++ destructureObjects/class7.js
@@ -0,0 +1,19 @@
+var sacha = {
+  nombre: 'Sacha',
+  apellido: 'Lifszyc',
+  edad: 28
+};
+var dario = {
+  nombre: 'Darío',
+  apellido: 'Susnisky',
+  edad: 27
+};
+function imprimirNombreEnMayusculas(persona) {
+  // var nombre = persona.nombre
+  var { nombre } = persona;
+  console.log(nombre.toUpperCase());
+}
+imprimirNombreEnMayusculas(sacha);
+imprimirNombreEnMayusculas(dario);
+// imprimirNombreEnMayusculas({ nombre: 'Pepito' })
+// imprimirNombreEnMayusculas({ apellido: 'Gomez' })
